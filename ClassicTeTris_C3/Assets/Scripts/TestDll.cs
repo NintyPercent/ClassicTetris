c4f29ccecc247e52302a9acdb954271cef4e6e42
Kev <NightElfKev@outlook.com>
2019-05-20 08:11:17 +0800

    第一波提交，准备工作

---
+++ ClassicTeTris_C3/Assets/Scripts/TestDll.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MK_PureMVC.Patterns.Facade;
+
+public class TestDll : Facade {
+
+    protected override void InitializeFacade()
+    {
+        base.InitializeFacade();
+    }
+}
